9e05d63912f21998eb90b5ed88e09a92a222088d
AndyL86 <alinehan89@outlook.com>
2022-07-27 20:26:55 +0000

    Added html user images and new javascript const variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,42 +3,9 @@
  * and possible choices
  */
 const buttons = document.getElementsByClassName("choice");
-const userScore = document.getElementById("user-score");
-const compScore = document.getElementById("comp-score");
-const alert = document.getElementById("results");
+const userWins = document.getElementById("user-score");
+const compWins = document.getElementById("comp-score");
+const userImage = document.getElementById("user-image");
+const compImage = document.getElementById("comp-image");
+const results = document.getElementById("result");
 const options = ["rock", "paper", "scissors", "lizard", "spock"];
-
-/**
- * Add event listener to all buttons
- */
-for (let button of buttons) {
-    button.addEventListener("click", function () {
-        let userChoice = this.getAttribute("data-choice");
-        playGame(userChoice);
-    });
-}
-
-/**
- * Function to start playing game
- */
-
-
-
-/**
- * Gets the current number of wins from the DOM and increments it by 1
- */
-function incrementWins() {
-
-    let oldScore = parseInt(document.getElementById("user-score").innerText);
-    document.getElementById("user-score").innerText = ++oldScore;
-
-}
-
-/**
- * Gets the current tally of losses from the DOM and increments it by 1
- */
-function incrementLosses() {
-
-    let oldScore = parseInt(document.getElementById("comp-score").innerText);
-    document.getElementById("comp-score").innerText = ++oldScore;
-}
