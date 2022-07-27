3fabd8b25a94fd2143b8ab659bf582d2bf13727a
AndyL86 <alinehan89@outlook.com>
2022-07-27 19:45:48 +0000

    Deleted code from JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,14 @@
 const buttons = document.getElementsByClassName("choice");
 const userScore = document.getElementById("user-score");
 const compScore = document.getElementById("comp-score");
-const alert = document.getElementById("result");
+const alert = document.getElementById("results");
 const options = ["rock", "paper", "scissors", "lizard", "spock"];
 
 /**
  * Add event listener to all buttons
  */
 for (let button of buttons) {
-    button.addEventListener("click", function() {
+    button.addEventListener("click", function () {
         let userChoice = this.getAttribute("data-choice");
         playGame(userChoice);
     });
@@ -22,13 +22,12 @@ for (let button of buttons) {
  * Function to start playing game
  */
 
- const choiceNumber = Math.floor(Math.random()*5);
- const computerChoice = options[choiceNumber];
+
 
 /**
  * Gets the current number of wins from the DOM and increments it by 1
  */
- function incrementWins() {
+function incrementWins() {
 
     let oldScore = parseInt(document.getElementById("user-score").innerText);
     document.getElementById("user-score").innerText = ++oldScore;
@@ -43,4 +42,3 @@ function incrementLosses() {
     let oldScore = parseInt(document.getElementById("comp-score").innerText);
     document.getElementById("comp-score").innerText = ++oldScore;
 }
-
