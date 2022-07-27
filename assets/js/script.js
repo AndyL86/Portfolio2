db2e07268e0a24db2f6aa2b1b6c959174dc2d9bf
AndyL86 <alinehan89@outlook.com>
2022-07-27 17:52:12 +0000

    Added incrementing score js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,22 @@ for (let button of buttons) {
         playGame(userChoice);
     });
 }
+
+/**
+ * Gets the current number of wins from the DOM and increments it by 1
+ */
+ function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("user-score").innerText);
+    document.getElementById("user-score").innerText = ++oldScore;
+
+}
+
+/**
+ * Gets the current tally of losses from the DOM and increments it by 1
+ */
+function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("sheldon-score").innerText);
+    document.getElementById("sheldon-score").innerText = ++oldScore;
+}
