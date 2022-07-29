864cfb5ca03cd697713d6c81150c00647511a9e7
AndyL86 <alinehan89@outlook.com>
2022-07-29 11:43:12 +0000

    moved image const

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@
 const buttons = document.getElementsByClassName("choice");
 const userWins = document.getElementById("user-score");
 const compWins = document.getElementById("comp-score");
+const userImage = document.getElementById("user-image");
+const compImage = document.getElementById("comp-image");
 const message = document.getElementById("alert").innerHTML;
 let options = ["rock", "paper", "scissors", "lizard", "spock"];
 
@@ -23,8 +25,6 @@ for (let button of buttons) {
  * The main game function 
  */
 function playGame(userChoice) {
-  const userImage = document.getElementById("user-image");
-const compImage = document.getElementById("comp-image");
 
   userImage.src = 'assets/images/${options[userChoice]}.png';
   userImage.alt = options[userChoice];
