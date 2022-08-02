06c84da92f8fb9979e4c0535c94345d6a5fcb7b6
AndyL86 <alinehan89@outlook.com>
2022-08-02 13:02:07 +0000

    fixed image seletion bug to display correct images

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,71 +2,77 @@
  * Declare constants for DOM elements
  * and possible choices
  */
-const buttons = document.getElementsByClassName("choice");
-var userWins = document.getElementById("user-score");
-var compWins = document.getElementById("comp-score");
-var userImage = document.getElementById("user-image");
-var compImage = document.getElementById("comp-image");
-var message = document.getElementById("alert");
-const options = ["rock", "paper", "scissors", "lizard", "spock"];
-
-
-/**
- * Add event listener to buttons
- */
-for (let button of buttons) {
-    button.addEventListener("click", function () {
-        var userChoice = this.getAttribute("data-choice")
-        playGame(userChoice);
-    });
-}
-
-/**
- * The main game function 
- */
-function playGame(userChoice) {
-
-    userImage.src = 'assets/${options[userChoice]}.png';
-    userImage.alt = options[userChoice];
-
-    var compChoice = Math.floor(Math.random() * 5);
-    compImage.src = 'assets/images/${options[compChoice]}.png';
-    compImage.alt = options[compChoice];
-
-    compareChoices(userChoice, compChoice);
-
-}
-
-
-/**
- * Checks to see who the winner is and game logic
- */
-
-function compareChoices(userChoice, compChoice) {
-
-    if (userChoice === compChoice) {
-        message.innerHTML = "Match is a Tie!";
-    } else if (userChoice === "rock" && compChoice === "scissors") {
-        message.innerHTML = "You Win!";
-    } else if (userChoice === "paper" && compChoice === "rock") {
-        message.innerHTML = "You Win!";
-    } else if (userChoice === "scissors" && compChoice === "paper") {
-        message.innerHTML = "You Win!";
-    } else if (userChoice === "rock" && compChoice === "lizard") {
-        message.innerHTML = "You Win!";
-    } else if (userChoice === "lizard" && compChoice === "spock") {
-        message.innerHTML = "You Win!";
-    } else if (userChoice === "spock" && compChoice === "scissors") {
-        message.innerHTML = "You Win!";
-    } else if (userChoice === "scissors" && compChoice === "lizard") {
-        message.innerHTML = "You Win!";
-    } else if (userChoice === "lizard" && compChoice === "paper") {
-        message.innerHTML = "You Win!";
-    } else if (userChoice === "paper" && compChoice === "spock") {
-        message.innerHTML = "You Win!";
-    } else if (userChoice === "spock" && compChoice === "rock") {
-        message.innerHTML = "You Win!";
-    } else {
-        message.innerHTML = "You Lose!";
-    }
-}
+ const buttons = document.getElementsByClassName("choice");
+ var userWins = document.getElementById("user-score");
+ var compWins = document.getElementById("comp-score");
+ const userImage = document.getElementById("user-image");
+ const compImage = document.getElementById("comp-image");
+ var message = document.querySelector("#alert");
+ var options = [];
+ 
+ options[0] = 'assets/images/rock.png';
+ options[1] = 'assets/images/paper.png';
+ options[2] = 'assets/images/scissors.png';
+ options[3] = 'assets/images/lizard.png';
+ options[4] = 'assets/images/spock.png';
+ 
+ 
+ /**
+  * Add event listener to buttons
+  */
+ for (let button of buttons) {
+     button.addEventListener("click", function () {
+         var userChoice = this.getAttribute("data-choice")
+         playGame(userChoice);
+     });
+ }
+ 
+ /**
+  * The main game function 
+  */
+ function playGame(userChoice) {
+ 
+     userImage.src = options[userChoice];
+     userImage.alt = options[userChoice];
+ 
+     const compChoice = Math.trunc(Math.random() * 5);
+     compImage.src = options[compChoice];
+     compImage.alt = options[compChoice];
+ 
+     compareChoices(userChoice, compChoice);
+ 
+ }
+ 
+ 
+ /**
+  * Checks to see who the winner is and game logic
+  */
+ 
+ function compareChoices(userChoice, compChoice) {
+ 
+     if (userChoice === compChoice) {
+         message.innerHTML = "Match is a Tie!";
+     } else if (userChoice === 0 && compChoice === options[compChoice]) {
+         message.innerHTML = "You Win!";
+     } else if (userChoice === 1 && compChoice === options[compChoice]) {
+         message.innerHTML = "You Win!";
+     } else if (userChoice === 2 && compChoice === options[compChoice]) {
+         message.innerHTML = "You Win!";
+     } else if (userChoice === 0 && compChoice === options[compChoice]) {
+         message.innerHTML = "You Win!";
+     } else if (userChoice === 3 && compChoice === options[compChoice]) {
+         message.innerHTML = "You Win!";
+     } else if (userChoice === 4 && compChoice === options[compChoice]) {
+         message.innerHTML = "You Win!";
+     } else if (userChoice === 2 && compChoice === options[compChoice]) {
+         message.innerHTML = "You Win!";
+     } else if (userChoice === 3 && compChoice === options[compChoice]) {
+         message.innerHTML = "You Win!";
+     } else if (userChoice === 1 && compChoice === options[compChoice]) {
+         message.innerHTML = "You Win!";
+     } else if (userChoice === 4 && compChoice === options[compChoice]) {
+         message.innerHTML = "You Win!";
+     } else {
+         message.innerHTML = "You Lose!";
+     }
+ }
