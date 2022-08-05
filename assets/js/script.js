0c1ab8ac81dda4d9aca46bd3397ff3b4ebc10ea7
AndyL86 <alinehan89@outlook.com>
2022-08-05 08:49:40 +0000

    changed var variables to const

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,18 +4,12 @@
  */
 
  const buttons = document.getElementsByClassName("choice");
- var userWins = document.getElementById("user-score");
- var compWins = document.getElementById("comp-score");
+ const userWins = document.getElementById("user-score");
+ const compWins = document.getElementById("comp-score");
  const userImage = document.getElementById("user-image");
  const compImage = document.getElementById("comp-image");
  
- var options = [];
  
- options[0] = 'assets/images/rock.png';
- options[1] = 'assets/images/paper.png';
- options[2] = 'assets/images/scissors.png';
- options[3] = 'assets/images/lizard.png';
- options[4] = 'assets/images/spock.png';
  
  let computerScore = 0;
  let playerScore = 0;
